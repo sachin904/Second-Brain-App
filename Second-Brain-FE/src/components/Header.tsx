6e4250a69671f7cbaf227631159ad28d8d93638d
sachin904 <kssachin904@gmail.com>
2025-06-12 18:21:07 +0530

    added logout button

--- Second-Brain-FE/src/components/Header.tsx
+++ Second-Brain-FE/src/components/Header.tsx
@@ -1,5 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import imgsrc from "../icons/brain.png"
+import { Button } from "./Button";
+import { LogoutIcon } from "../icons/LogoutIcon";
 export function Header(){
+const navigate=useNavigate();
+  const token = localStorage.getItem("token");
+   const handleLogout = () => {
+    localStorage.removeItem("token");
+      navigate("/secondBrain/signin"); // or use navigation if using React Router
+  };
    return<>
      <div id="header" className="w-screen h-12 bg-blue-500 flex justify-center items-center">
 <div className="flex items-center gap-1">
@@ -10,6 +19,18 @@ export function Header(){
     <p className="text-white">Second</p>
     <p className="text-white">Brain</p>
   </div>
+
+      {token && token !== "undefined" && (
+        <div className="absolute right-4">
+        <Button
+          variant="primary"
+          
+          text="Logout"
+          onClick={handleLogout}
+          startIcon={<LogoutIcon/>}
+        />
+        </div>
+           )}
 </div>
 
 
